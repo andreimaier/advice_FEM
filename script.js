e9d5dbada3321fca9aea984ae55e6184e631baa8
andreimaier <113696878+andreimaier@users.noreply.github.com>
2023-07-10 11:30:46 +0800

    initial commit
    completed challenge. working on customization

---
+++ script.js
@@ -0,0 +1,19 @@
+const id = document.querySelector('#id')
+const sfat = document.querySelector('#sfat')
+const button = document.querySelector('#button')
+const baba = document.querySelector('#lala')
+
+async function getAdvice() {
+    const response = await fetch("https://api.adviceslip.com/advice?t=" + Math.random());
+    const advice = await response.json();
+
+    id.textContent = `ADVICE #${advice.slip.id}`
+    sfat.textContent = `"${advice.slip.advice}"`
+
+    // console.log(advice.slip);
+}
+
+button.addEventListener('click', () => getAdvice())
+
+
+
