1dd510cf6ec19a060bb34828e8885c592ed4567e
andreimaier <113696878+andreimaier@users.noreply.github.com>
2023-07-13 10:31:40 +0800

    width update

--- script.js
+++ script.js
@@ -6,12 +6,10 @@ let response = ''
 async function getAdvice() {
     const request = await fetch("https://api.adviceslip.com/advice?t=" + Math.random());
     response = await request.json();
-
-    console.log(response.slip);
 }
+
 async function giveAdvice() {
     await getAdvice()
-
     id.textContent = `ADVICE #${response.slip.id}`
     sfat.textContent = `"${response.slip.advice}"`
 }
